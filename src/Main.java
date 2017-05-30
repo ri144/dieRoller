03aff6264982aa8cb2c00b01ee00108534b7f5f4
Rob Gross <144big@gmail.com>
2017-05-30 10:49:03 -0400

    first commit for 6-sided die

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Main {
+	public static void main( String[] args )
+    {
+        int dienumber;
+        Random rnd = new Random();
+        
+        dienumber = rnd.nextInt(6) + 1;
+        
+        System.out.println("Your die roll was : " + dienumber);
+    }
+}
